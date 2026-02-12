df400076ed466716374f692cb00f816a2fc8bdd4
kasssas <elkassasmostafa29@gmail.com>
2026-02-12 23:04:03 +0200

    fix(rds): downgrade postgres engine version to 16.1

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -35,7 +35,7 @@ resource "aws_db_instance" "backend" {
   identifier             = "backend-db"
   allocated_storage      = 20
   engine                 = "postgres"
-  engine_version         = "16.3"
+  engine_version         = "16.1"
   instance_class         = "db.t4g.micro"
   db_name                = "backend"
   username               = "appuser"
